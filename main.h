96dd1c17a1c65f1806b02233bcfd0614d162f201
Chris Stamper <cstampok@gmail.com>
2022-11-08 08:03:45 -0600

    create and test string duplicator

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*
+ * libraries - to be included
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/*
+ * prototypes - function protos below
+ */
+char *dupstr(char *fire);
+
+#endif
