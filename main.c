a85cba00953e52f401481026cad808be0303aacc
Chris Stamper <cstampok@gmail.com>
2022-11-27 15:26:13 -0600

    wait test

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 
 int main(void)
 {
-    pid_t child_pid = fork();
+    pid_t child_pid, hold_up;
+	int stat1;
 
+	child_pid = fork();
     if (child_pid == 0)
 	{
         printf("### Child ###\nCurrent PID: %d and Child PID: %d\n",
                getpid(), child_pid);
-    } else
-	{
 		sleep(1);
+    }
+	else
+	{
+		hold_up = waitpid(child_pid, &stat1, WUNTRACED);
+		if (hold_up)
+			printf("SUCCESSFUL HOLD\n");
         printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n",
                getpid(), child_pid);
     }
- 
     return (0);
 }
--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <sys/wait.h>
 
 /*
  * prototypes - function protos below
